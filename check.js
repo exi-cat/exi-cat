bb1bc4bc20c0c50e7c11633ed85f6d9fe26ae220
exi-cat <83468106+exi-cat@users.noreply.github.com>
2021-05-08 18:35:12 +0300

    Create check.js

---
+++ check.js
@@ -0,0 +1,53 @@
+/* let validInput = 5;
+
+
+function numInput() {
+    let number = validInput;
+    return number;
+}
+
+console.log(numInput());
+
+function checkInput() {
+    let check;
+    // let number = numInput();
+    // console.log(number);
+        if (numInput() == Number(numInput())) {
+            console.log('CORRECT!');
+            return check = true;
+        } else {
+            console.log('NOT NUMBER');
+            // return check = false;
+    }
+}
+
+function showResult() {
+    numInput();
+    // checkInput();
+    if (checkInput() == true) {
+        console.log(numInput() + " Sucess!!!");
+    } else {
+        console.log('MISTAKE');
+    }
+}
+
+showResult(); */
+
+function sum() {
+    let a = 3;
+    let b = 2;
+    return result = a + b;
+}
+function show() {
+    if (result > 6) {
+        return answer = 'bingo';
+    } else {
+        return answer = 'fail';
+    }
+}
+function main() {
+    sum();
+    show();
+}
+main();
+console.log(answer);
